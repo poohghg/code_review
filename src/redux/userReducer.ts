164b09883e0065f4a4b1ddf5b6aca01323367f7a
khg <poohghg@naver.com>
2023-02-26 22:55:47 +0900

    수정

--- src/redux/userReducer.ts
+++ src/redux/userReducer.ts
@@ -1,6 +1,5 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { CartType } from "../graphql/gqlCart";
-import { Addresses, User } from "../graphql/gqlUser";
+import { Addresses } from "../graphql/gqlUser";
 
 // https://kyounghwan01.github.io/blog/React/redux/redux-toolkit/#%E1%84%89%E1%85%A1%E1%84%8B%E1%85%AD%E1%86%BC%E1%84%92%E1%85%A1%E1%84%82%E1%85%B3%E1%86%AB-%E1%84%8B%E1%85%B5%E1%84%8B%E1%85%B2
 interface UserType {
